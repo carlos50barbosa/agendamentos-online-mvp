8ffad2f5554d81750b629789860116c20827ea89
carlo50barbosa <carlos50barbosa@gmail.com>
2026-01-01 01:03:50 -0300

    Atualizações 01-01-2026

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+  build: {
+    target: 'es2017',
+  },
   server: { port: 3001 }
 })
